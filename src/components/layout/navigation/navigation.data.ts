009658ca5144f5d9fc02de9ba163196ad8b232b2
Timur <mynameistimurgafurov@gmail.com>
2024-08-19 15:21:03 +0300

    parse

--- src/components/layout/navigation/navigation.data.ts
+++ src/components/layout/navigation/navigation.data.ts
@@ -1,12 +1,13 @@
 import home from '@/assets/images/navigation/home.svg'
 import nft from '@/assets/images/navigation/nft.svg'
 import staking from '@/assets/images/navigation/staking.svg'
+import tasks from '@/assets/images/navigation/tasks.svg'
 
 import { NavItem } from './navigation.types'
 
 export const navItems: NavItem[] = [
 	{ to: `/`, icon: home, label: 'Home' },
-	{ to: `/tasks`, icon: nft, label: 'Staking' },
+	{ to: `/tasks`, icon: tasks, label: 'Tasks' },
 	{ to: `/staking`, icon: staking, label: 'Staking' },
 	{ to: `/profile`, icon: nft, label: 'NFT' }
 ]
