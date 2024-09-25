4526a33388ede10eb194c3979529034d396dadb3
timurimba <mynameistimurgafurov@gmail.com>
2024-09-25 12:21:20 +0300

    fix

--- src/consts/consts.ts
+++ src/consts/consts.ts
@@ -1,12 +1,6 @@
 export const telegramId = String(window.Telegram.WebApp.initDataUnsafe.user!.id)
 export const DURATION_TIMER = 28800
 
-// window.Telegram.WebApp.initDataUnsafe.user!.id
-//384432527
-
-// export const BUY_TEXT = "Buy 100000 NP = 70 TON";
-// export const SALE_TEXT = "Sell 100000 NP = 50 TON";
-
 export const BUY_NP = 10000
 export const SELL_NP = 100000
 export const BUY_TEXT_SHORT = 'Buy'
