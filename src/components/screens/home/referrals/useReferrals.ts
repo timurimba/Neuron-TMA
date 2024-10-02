8a0211bf5ea77d5eeab9cc639ac2e98d7f5ae57d
timurimba <mynameistimurgafurov@gmail.com>
2024-10-02 18:13:31 +0300

    change text referal link

--- src/components/screens/home/referrals/useReferrals.ts
+++ src/components/screens/home/referrals/useReferrals.ts
@@ -19,7 +19,7 @@ export const useRefferals = () => {
 
 	const inviteFriend = () => {
 		window.open(
-			`https://t.me/share/url?url=${encodeURIComponent(`Hi, here is a referral link to our App: https://t.me/Neuron_ton_bot?start=${telegramId}`)}`,
+			`https://t.me/share/url?url=${encodeURIComponent(`Hi, Neuronian! Get 3000 NP for using my referral link: https://t.me/Neuron_ton_bot?start=${telegramId}`)}`,
 			'_blank'
 		)
 	}
--- src/components/screens/profile/buy-nft/useBuyNft.ts
+++ src/components/screens/profile/buy-nft/useBuyNft.ts
@@ -1,16 +1,36 @@
-import { useMutation } from '@tanstack/react-query'
+import { useMutation, useQuery } from '@tanstack/react-query'
 import { toNano } from '@ton/core'
 import { useMemo } from 'react'
 
 import { TonService } from '@/services/ton/ton.service'
+import { UserService } from '@/services/user/user.service'
 
+import { queryClient } from '@/providers/tanstack/TanstackProvider'
+
+import { IUser } from '@/types/user.types'
+
+import { useStartInterval } from '@/hooks/useStartInterval'
 import { useWallet } from '@/hooks/useWallet'
+
+import { telegramId } from '@/consts/consts'
+import { useIntervalStore, usePointsStore } from '@/store/store'
 
 const images: any = import.meta.glob('@/assets/images/nft/*.png', {
 	eager: true
 })
 
 export const useBuyNft = () => {
+	const { data: user } = useQuery({
+		queryKey: ['get-user'],
+		queryFn: () => UserService.getUserFields<IUser>(telegramId)
+	})
+
+	const { intervalId } = useIntervalStore(state => state)
+
+	UserService.setCountDownTimer(telegramId)
+
+	const { startInterval } = useStartInterval()
+
 	const randomImage: any = useMemo(() => {
 		const keys = Object.keys(images)
 		const randomIndex = Math.floor(Math.random() * keys.length)
@@ -46,6 +66,15 @@ export const useBuyNft = () => {
 					wallet: wallet!,
 					randomNftAddress
 				})
+				await UserService.setIsHadNft(telegramId, true)
+				queryClient.invalidateQueries({
+					queryKey: ['get-is-had-nft']
+				})
+				if (user && user.startTimer) {
+					UserService.updatePoints(telegramId, usePointsStore.getState().points)
+					clearInterval(intervalId!)
+					startInterval(0.01)
+				}
 			}
 		} catch (error: any) {
 			console.log(error)
