95909f518e98f3c3f0ac53dd22957ec46d086e59
timurimba <mynameistimurgafurov@gmail.com>
2024-10-16 19:02:44 +0300

    fix

--- src/components/screens/profile/buy-nft/BuyNft.tsx
+++ src/components/screens/profile/buy-nft/BuyNft.tsx
@@ -1,6 +1,10 @@
+import { useQuery } from '@tanstack/react-query'
+import { fromNano } from '@ton/core'
 import { type FC } from 'react'
 
 import Button from '@/components/shared/button/Button'
+
+import { TonService } from '@/services/ton/ton.service'
 
 import tonImage from '@/assets/images/ton.png'
 
@@ -12,18 +16,22 @@ import { useBuyNft } from './useBuyNft'
 const BuyNft: FC = () => {
 	const { randomImage, buyNft } = useBuyNft()
 	const { connected, wallet } = useWallet()
-	// const { data: balance } = useQuery({
-	// 	queryKey: ['get-balance'],
-	// 	queryFn: () => TonService.getBalance(wallet!),
-	// 	enabled: !!wallet
-	// })
+	const { data: balance } = useQuery({
+		queryKey: ['get-balance'],
+		queryFn: () => TonService.getBalance(wallet!),
+		enabled: !!wallet
+	})
+
 	return (
 		<div className={styles.wrapper}>
 			<img src={randomImage} alt='' />
 			<p>
 				PRICE: 7 <img className='w-7 h-7' src={tonImage} alt='' />
 			</p>
-			<Button disabled={!connected} onClick={buyNft}>
+			<Button
+				disabled={!connected || (balance && Number(fromNano(balance)) < 7)}
+				onClick={buyNft}
+			>
 				buy
 			</Button>
 		</div>
